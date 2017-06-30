3710fe953ee6051379748c102d66e0444744faf3
Travis N. Thomsen <travis@linuxacademy.com>
2017-06-30 15:59:40 -0400

    remove selinux

--- tests/apache.pp
+++ tests/apache.pp
@@ -1,1 +1,8 @@
-include ::profile::apache
+class { '::profile::apache': 
+  apache_vhost_servers => {
+    'tthomsen2.mylabserver.com' => {
+      port    => 80,
+      docroot => '/var/www/tthomsen2.mylabserver.com'
+    }
+  }
+}
